ff03b9b4e82353ed595813b25f0ca1d36255ceaa
sandeep <sandeep.baradi@gmail.com>
2025-06-08 09:33:28 +0530

    update settings Again!!

--- ecom/settings.py
+++ ecom/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'HOST': 'postgres.railway.internal',
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'PORT': '5432',
+        'NAME': os.getenv('PGDATABASE'),
+        'HOST': os.getenv('PGHOST'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
